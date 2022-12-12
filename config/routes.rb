206ccecd59f1030c496f48af16267abcdab5c571
ek4aa <eastking4aa@gmail.com>
2022-12-13 00:37:29 +0800

    Add: Specifying Nested Routes and Associations for the Parent Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :sharks
+  resources :sharks do
+    resources :posts
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
